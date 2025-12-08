b2f7612859cc0fc45b42dd9fed439f8df24b0adf
VgneshPragalathan <vigneshpragalathan1999@gmail.com>
2025-12-08 18:03:22 +0530

    Workflow added

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-print("Hello World!!")
+# output will be written to result.txt
 
+message = "Hello, this is the output!"
+
+with open("result.txt", "w") as file:
+    file.write(message)
+
+print("Output written to result.txt")
