aea584a6589b35b39fac5505f2c6d7b337b82ec5
VgneshPragalathan <vigneshpragalathan1999@gmail.com>
2025-12-08 18:13:16 +0530

    Workflow added

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 # output will be written to result.txt
+import os
 
 message = "Hello, this is the output!"
+
+os.makedirs('src',exist_ok=True)
 
 with open("src/result.txt", "w") as file:
     file.write(message)
